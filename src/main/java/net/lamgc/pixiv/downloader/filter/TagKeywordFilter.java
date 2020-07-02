9f651792dd9391a6c51e051ec983484a9896699a
LamGC <lam827@lamgc.net>
2020-07-02 12:54:27 +0800

    [Add] TagKeywordFilter 增加正则匹配功能;

--- src/main/java/net/lamgc/pixiv/downloader/filter/TagKeywordFilter.java
+++ src/main/java/net/lamgc/pixiv/downloader/filter/TagKeywordFilter.java
@@ -1,6 +1,13 @@
 package net.lamgc.pixiv.downloader.filter;
 
+import java.util.regex.Pattern;
+
+/**
+ * Tag关键字过滤器.
+ */
 public class TagKeywordFilter implements IllustFilter {
+
+    private final boolean useRegex;
 
     private final String targetTag;
 
@@ -8,23 +15,58 @@ public class TagKeywordFilter implements IllustFilter {
 
     private final boolean fullMatch;
 
+    private final Pattern matchPattern;
+
+    /**
+     * 使用关键词构造Tag过滤器
+     * @param targetTag 目标关键词
+     * @param ignoreCase 是否忽略大小写
+     * @param fullMatch 是否完全匹配
+     */
     public TagKeywordFilter(String targetTag, boolean ignoreCase, boolean fullMatch) {
-        this.targetTag = targetTag;
+        this.useRegex = false;
         this.ignoreCase = ignoreCase;
+        this.targetTag = ignoreCase ? targetTag.toLowerCase() : targetTag;
         this.fullMatch = fullMatch;
+        this.matchPattern = null;
+    }
+
+    /**
+     * 使用正则表达式构建Tag过滤器
+     * @param matchPattern 正则表达式对象
+     */
+    public TagKeywordFilter(Pattern matchPattern) {
+        this.useRegex = true;
+        this.ignoreCase = false;
+        this.targetTag = null;
+        this.fullMatch = false;
+        this.matchPattern = matchPattern;
     }
 
     @Override
     public boolean filterIllust(int id, int pageCount, String title, String description, int userId, String fileExtName, String[] tags) {
+        return useRegex ? regexMatch(tags) : container(tags);
+    }
+
+    private boolean regexMatch(String[] tags) {
+        for(String tag : tags) {
+            if(matchPattern.matcher(tag).matches()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean container(String[] tags) {
         for (String tag : tags) {
             if(fullMatch ?
-                    ignoreCase ? tag.equalsIgnoreCase(targetTag) : tag.equals(targetTag) :
-                    tag.contains(targetTag)
+                    ( ignoreCase ? tag.equalsIgnoreCase(targetTag) : tag.equals(targetTag) ) :
+                    ( ignoreCase ? tag.toLowerCase().contains(targetTag) : tag.contains(targetTag) )
             ) {
                 return true;
             }
         }
-         return false;
+        return false;
     }
 
 }
